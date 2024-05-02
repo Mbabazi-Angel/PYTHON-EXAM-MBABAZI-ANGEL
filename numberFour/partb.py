bc0005067a73e8d8601a172f62f25b75f9265b39
Mbabazi-Angel <144022384+Mbabazi-Angel@users.noreply.github.com>
2024-05-02 12:50:10 +0300

    python exam

--- numberFour/partb.py
+++ numberFour/partb.py
@@ -1,53 +1,49 @@
 def wituSacco():
     accountBalance = 0
+    run = 1
     while run == 1:
         print("Welcome to WITU Sacco")
         
-#menu
-print('1.Deposit Money')
-print('2.Withdraw Money')
-print('3.Check Deposit')
+        #menu
+        print('1.Deposit Money')
+        print('2.Withdraw Money')
+        print('3.Check Deposit')
+        
+        studentChoice = int(input("Enter choice 1, 2, or 3: "))
+        
+        if studentChoice == 1:
+            print('\n... Processing a deposit transaction...')
+            depositAmount = int(input("Enter deposit amount: "))
+            
+            if depositAmount < 1000:
+                print("\nMinimum deposit is 1000")
+            else:
+                accountBalance += depositAmount
+                print(f"Dear student, you have deposited {depositAmount} and your new balance is {accountBalance}")
 
-studentChoice = int(input("Enter choice 1,2,3: "))
-print('\n... Processing a deposit transaction...')
+        elif studentChoice == 2:
+            print('\n...Processing a withdraw transaction...')
+            withdrawAmount = int(input("Enter withdraw amount: "))
+            if accountBalance == 0:
+                print('Your account balance is 0')
+            elif withdrawAmount < 500:
+                print("Minimum withdraw amount is 500")
+            elif withdrawAmount > accountBalance:
+                print('Withdraw failed, insufficient funds')
+            else:
+                accountBalance -= withdrawAmount
+                print(f'Dear student, you have withdrawn {withdrawAmount}') 
 
-if studentChoice == 1:
-    print('\n...Processing a deposit transaction')
-    depositAmount =int(input("Enter deposit amount: "))
-    
-    if depositAmount<1000:
-        print("\n Minimum deposit is 1000")
-    else:
-        accountBalance =+ depositAmount
-        print(f"Dear student, you have deposited {depositAmount} and your new balance is {accountBalance}")
+        elif studentChoice == 3:
+            print('\n...Processing a check deposit transaction...')
+            print(f'Your account balance is {accountBalance}')    
 
-elif studentChoice == 2:
-    print('\n..Processing a withdraw transaction...')
-    withdrawAmount = int(input("Enter withdraw amount:"))
-    if accountBalance == 0:
-        print('Your account balance is 0 ')
-    elif withdrawAmount < 500:
-        print("Minimum withdraw amount is 500")
-    elif withdrawAmount > accountBalance:
-        print(f'Withdraw failed, inssufficient funds')
-    else:
-        accountBalance += withdrawAmount
-        print(f'Dear student, you have withdrawn {withdrawAmount}') 
+        else:
+            print("You entered a wrong choice! Please select 1, 2, or 3")
+            
+        run = int(input("Enter 1 to continue or any number to exit: "))
 
-elif studentChoice == 3:
-    print('\n... Processing a check deposit transaction...')
-    if depositAmount<1000:
-        print("\n Minimum deposit is 1000")
-    else:
-        print(f'\n Your account balance is {accountBalance}')    
-
-else:
-    print("You entered a wrong choice! please select 1,2 or 3")
-    
-run = int(input("Enter 1 to continue or any numer to exit"))
-
-if run != 1:
     print("Thanks for using WITU Sacco")
 
 wituSacco()
-break
+
